8721cd4984bc00b5e25dd9d457d22a2083c1d9ff
rachel2727 <rhs144@usf.edu>
2024-09-08 20:37:03 +0000

    Newest Update to test sep 8

--- test1.c
+++ test1.c
@@ -3,4 +3,17 @@
 int main()
 {
     printf("Oh");
+
+    enum flavors{vanilla, chocolate, mint};
+    enum flavors i = vanilla;
+
+    float price_of_flavors[3];
+
+    printf("This is the first flavors price:\n");
+    float sales;
+    scanf("%f", &sales);
+    price_of_flavors[i] = sales;
+
+    
+
 }
