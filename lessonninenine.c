bd88aa7683fb0cc142f424e50191aaeac81fb361
rachel2727 <rhs144@usf.edu>
2024-09-09 12:20:03 +0000

    2nd commit ninenine

--- lessonninenine.c
+++ lessonninenine.c
@@ -2,6 +2,26 @@
 
 int main()
 {
-    int m[5][9]; //five rows nine columns.
-    
+    int m[5][9]; //five rows nine columns, or five rows of nine values.
+
+    const char chars[] = {'0', '1'};
+
+    char grade = 'A';
+
+    switch (grade) {
+        case 'A': printf("Excellent\n");
+                break;
+        case 'B': printf("Good\n");
+                break;
+        case 'C': printf("Average\n");
+                break;
+        case 'D': printf("Poor\n");
+                break;
+        case 'F': printf("Failing\n");
+                break;
+        default: printf("Illegal grade\n");
+                break;
+
+        printf("\nAll done");
+    }
 }
