b19e076aa575bbbe36f7e4f5ac25032733e9ec6c
rachel2727 <rhs144@usf.edu>
2024-09-17 01:17:50 +0000

    HW1 initial draft

---
+++ HW1.c
@@ -0,0 +1,28 @@
+/*
+COP 3515 - Fall Semester 2024
+
+Homework #1: Password Strength Meter
+
+Rachel Singh
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+int main()
+{
+    int maxPass = 25;
+    int minPass = 12;
+    bool passPhrases = true; 
+    bool optTests = true;
+
+    FILE *passFile; 
+
+    passFile = fopen("Proposed passwords #1.txt", "r");
+
+    
+
+
+}
